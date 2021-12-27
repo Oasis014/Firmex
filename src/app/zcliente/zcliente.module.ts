220044ca55c5da6c71c9f1f6e5ade8bcc7c6bfc7
Jonathan Leon <jonathan_leonl@hotmail.com>
2021-12-27 16:38:27 -0600

    errors solved

--- src/app/zcliente/zcliente.module.ts
+++ src/app/zcliente/zcliente.module.ts
@@ -3,8 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NgbProgressbarModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { MatTableModule } from '@angular/material/table'
-import {DataSource} from '@angular/cdk/collections';
+//import {DataSource} from '@angular/cdk/collections';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ClienteRoutingModule } from './zcliente-routing.module';
@@ -46,7 +45,6 @@ import { PhpPruebaComponent } from './php-prueba/php-prueba.component';
 
 ],
   imports: [
-  MatTableModule,
     CommonModule,
     ClienteRoutingModule,
     NgbModule,
